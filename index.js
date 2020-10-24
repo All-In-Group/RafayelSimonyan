3f8717b23fc5ee35ed4e060a054509c78c2a2131
RafoSimonyan7 <70983991+RafoSimonyan7@users.noreply.github.com>
2020-10-24 04:42:36 +0300

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function getToggle(e) {
+    document.querySelector('.modal-bg').classList.add('active');
     document.querySelectorAll(".box").forEach(box => box.style.display = "none");
     document.getElementById(e).style.display = "block";
-    
+}
+
+function closeModal() {
+    const close = document.querySelector('.modal-bg')
+    close.classList.remove('active')
 }
